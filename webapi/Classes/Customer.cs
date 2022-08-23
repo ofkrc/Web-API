27622c38221643186f732434f23f2bab3d6fa2d0
ofkrc <75641524+ofkrc@users.noreply.github.com>
2022-08-23 16:00:38 +0300

    JSON is used as database.

--- webapi/Classes/Customer.cs
+++ webapi/Classes/Customer.cs
@@ -8,10 +8,6 @@
         public string? CustomerName { get; set; }
         public string? Description { get; set; }
         public int Status { get; set; }      
-        
-        
-
-        
 
     }
 }
